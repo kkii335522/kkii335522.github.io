7d176091bc9f929d626ade9e46294878186c5ec3
TKU Course Application <tku.course.assistant@gmail.com>
2018-07-29 19:24:44 +0800

    update index.html

--- assets/js/script.en.js
+++ assets/js/script.en.js
@@ -2,66 +2,92 @@ var app = angular.module("sunnyweb", ["ngRoute"]);
 app.config(function($routeProvider) {
     $routeProvider
     .when("/en-us", {
-        templateUrl : "views/en-us/index.html"
+        templateUrl : "views/en-us/index.html",
+		controller : "Ctrl"
     })
 	.when("/en-us/contactus", {
-        templateUrl : "views/en-us/contactus.html"
+        templateUrl : "views/en-us/contactus.html",
+		controller : "Ctrl"
     })
 	.when("/en-us/news", {
-        templateUrl : "views/en-us/news.html"
+        templateUrl : "views/en-us/news.html",
+		controller : "Ctrl"
     })
 	.when("/en-us/autocontrol", {
-        templateUrl : "views/en-us/autocontrol/p1.html"
+        templateUrl : "views/en-us/autocontrol/p1.html",
+		controller : "Ctrl"
     })
 	.when("/en-us/autocontrol/p1", {
-        templateUrl : "views/en-us/autocontrol/p1.html"
+        templateUrl : "views/en-us/autocontrol/p1.html",
+		controller : "Ctrl"
     })
 	.when("/en-us/autocontrol/p2", {
-        templateUrl : "views/en-us/autocontrol/p2.html"
+        templateUrl : "views/en-us/autocontrol/p2.html",
+		controller : "Ctrl"
     })
 	.when("/en-us/autocontrol/p3", {
-        templateUrl : "views/en-us/autocontrol/p3.html"
+        templateUrl : "views/en-us/autocontrol/p3.html",
+		controller : "Ctrl"
     })
 	.when("/en-us/autocontrol/p4", {
-        templateUrl : "views/en-us/autocontrol/p4.html"
+        templateUrl : "views/en-us/autocontrol/p4.html",
+		controller : "Ctrl"
     })
 	.when("/en-us/autocontrol/p5", {
-        templateUrl : "views/en-us/autocontrol/p5.html"
+        templateUrl : "views/en-us/autocontrol/p5.html",
+		controller : "Ctrl"
     })
 	.when("/en-us/sensor", {
-        templateUrl : "views/en-us/sensor/p1.html"
+        templateUrl : "views/en-us/sensor/p1.html",
+		controller : "Ctrl"
     })
 	.when("/en-us/sensor/p1", {
-        templateUrl : "views/en-us/sensor/p1.html"
+        templateUrl : "views/en-us/sensor/p1.html",
+		controller : "Ctrl"
     })
 	.when("/en-us/sensor/p2", {
-        templateUrl : "views/en-us/sensor/p2.html"
+        templateUrl : "views/en-us/sensor/p2.html",
+		controller : "Ctrl"
     })
 	.when("/en-us/sensor/p3", {
-        templateUrl : "views/en-us/sensor/p3.html"
+        templateUrl : "views/en-us/sensor/p3.html",
+		controller : "Ctrl"
     })
 	.when("/en-us/sensor/p4", {
-        templateUrl : "views/en-us/sensor/p4.html"
+        templateUrl : "views/en-us/sensor/p4.html",
+		controller : "Ctrl"
     })
 	.when("/en-us/sensor/p5", {
-        templateUrl : "views/en-us/sensor/p5.html"
+        templateUrl : "views/en-us/sensor/p5.html",
+		controller : "Ctrl"
     })
 	.when("/en-us/sensor/p6", {
-        templateUrl : "views/en-us/sensor/p6.html"
+        templateUrl : "views/en-us/sensor/p6.html",
+		controller : "Ctrl"
     })
 	.when("/en-us/sensor/p7", {
-        templateUrl : "views/en-us/sensor/p7.html"
+        templateUrl : "views/en-us/sensor/p7.html",
+		controller : "Ctrl"
     })
 	.when("/en-us/sensor/p8", {
-        templateUrl : "views/en-us/sensor/p8.html"
+        templateUrl : "views/en-us/sensor/p8.html",
+		controller : "Ctrl"
     })
 	.when("/en-us/sensor/p9", {
-        templateUrl : "views/en-us/sensor/p9.html"
+        templateUrl : "views/en-us/sensor/p9.html",
+		controller : "Ctrl"
     })
 	.when("/en-us/sensor/p10", {
-        templateUrl : "views/en-us/sensor/p10.html"
+        templateUrl : "views/en-us/sensor/p10.html",
+		controller : "Ctrl"
     })
 	.otherwise({
-        templateUrl : "views/en-us/index.html"
+        templateUrl : "views/en-us/index.html",
+		controller : "Ctrl"
     });
+});
+
+app.controller("Ctrl", function ($scope) {
+	
+	alert(window.location.href);
 });
